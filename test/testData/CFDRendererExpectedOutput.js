1ceb50bdd18eb9b5c3c17a73403442b3586d1ef7
ClaudiaGivan <claudia-maria.givan@pfizer.com>
2024-06-16 16:39:44 +0300

    Add process behaviour charts

--- test/testData/CFDRendererExpectedOutput.js
+++ test/testData/CFDRendererExpectedOutput.js
@@ -9,12 +9,12 @@ const metricsOutput = {
         "analysis_done": 0,
         "analysis_active": 0
     },
-    "biggestCycleTime": 2,
-    "averageLeadTime": 3,
+    "biggestCycleTime": 3,
+    "averageLeadTime": 4,
     "throughput": 0.3,
     "metricLinesData": {
         "averageCycleTime": null,
-        "averageLeadTime": 3,
+        "averageLeadTime": 4,
         "leadTimeDateBefore": "2023-04-05T21:00:00.000Z",
         "cycleTimeDateBefore": null,
         "currentDate": "2023-04-09T00:00:00.000Z",
